a82bd17c19f1f9f43d81f88da88414362196b3ba
Leonardo Rivero <leonardorivero145@gmail.com>
2020-06-02 16:27:36 -0430

    Creo y agrego el componente Footer al componente _layout.svelte y agrego u logo de svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,47 @@
+<script>
+
+</script>
+
+<style>
+  footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 4em;
+    background: #dadada;
+  }
+  footer p {
+    font-weight: 500;
+    font-size: 1.3em;
+    margin: 0;
+  }
+  footer a {
+    font-weight: bold;
+  }
+  footer img {
+    height: 2.5em;
+  }
+  footer .credit-svelte {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  footer .credit-svelte p {
+    margin-right: 0.3em;
+  }
+</style>
+
+<footer>
+  <div class="container">
+    <div>
+      <p>
+        Designed with &#128154; by
+        <a href="/">@CodingLeonardo</a>
+      </p>
+    </div>
+    <div class="credit-svelte">
+      <p>Powered by</p>
+      <img src="/svelte.svg" alt="Svelte" />
+    </div>
+  </div>
+</footer>
