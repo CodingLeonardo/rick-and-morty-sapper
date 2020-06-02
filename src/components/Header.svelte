e11a8b1ecb9d83228d9c7aa3d1c33c83db83483a
Leonardo Rivero <leonardorivero145@gmail.com>
2020-06-01 22:36:21 -0430

    Agrego un logo y creo el componente Header al proyecto

---
+++ src/components/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<style>
+  header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #dadada;
+    height: 5em;
+  }
+  header a {
+    display: block;
+    height: 80%;
+  }
+  header img {
+    height: 100%;
+  }
+</style>
+
+<header>
+  <a href="/">
+    <img src="/logo.svg" alt="Rick And Morty" />
+  </a>
+</header>
